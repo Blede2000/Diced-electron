53c1603970b0c8aebcd32600ab04e7cbb99ae3a0
BuildTools <unconfigured@null.spigotmc.org>
2021-05-23 17:34:22 -0400

    Added login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { HashRouter, Switch, Route, Link } from "react-router-dom";
+import { HashRouter, Switch, Route, Link, useLocation } from "react-router-dom";
 import "./App.global.css";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import Sheet from "./screens/Sheet.jsx";
 import Selector from "./screens/Selector.jsx";
 import Sidebar from "./components/window/Sidebar.jsx";
+import Register from "./screens/Register.jsx";
 
 export default function App() {
     return (
@@ -15,6 +16,7 @@ export default function App() {
                 <Switch>
                     <Route exact path="/" component={Selector} />
                     <Route path="/character/:id" component={Sheet} />
+                    <Route path="/login" component={Register} />
                 </Switch>
             </HashRouter>
         </Provider>
